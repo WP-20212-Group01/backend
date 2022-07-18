108d70fb4a35d92086f6c82d9ae341f6c2577076
kcbt <kcbthasnoidea@gmail.com>
2022-07-18 23:57:26 +0700

    feat: add swagger integration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,15 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  const swaggerDocBuilder = new DocumentBuilder()
+    .setTitle('Candle shop')
+    .setDescription('Candle shop API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerDocBuilder);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
