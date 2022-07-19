a260cb2dd18f4573a335204156d7dd72f4ce3bdf
kcbt <kcbthasnoidea@gmail.com>
2022-07-19 12:11:02 +0700

    feat: add config module

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -5,12 +6,18 @@ import {
 } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigInterface } from './config/config.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const configService: ConfigService<ConfigInterface> = app.get(ConfigService);
+
+  const env = configService.get('NODE_ENV') || 'development';
+  if (env !== 'production') app.enableCors();
 
   const swaggerDocBuilder = new DocumentBuilder()
     .setTitle('Candle shop')
@@ -20,6 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerDocBuilder);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(+(configService.get('PORT') || '3000'));
 }
 bootstrap();
