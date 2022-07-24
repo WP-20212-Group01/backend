71d57417b37aeb7b5a41cf748b37337ae17e9a30
Tung Lam Tran <kcbthasnoidea@gmail.com>
2022-07-24 22:32:45 +0700

    chore: add tag to comment controller

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dtos/create-comment.dto';
 import { GetCommentDto } from './dtos/get-comment.dto';
 
+@ApiTags('comment')
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
